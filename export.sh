3ee7d352ba6ca44ecfbc67afe7d805dc5df76d5d
Sebastian Montillo <montillo214@gmail.com>
2025-12-08 10:55:07 -0500

    Auto-export from Bootstrap Studio

--- export.sh
+++ export.sh
@@ -1,5 +1,15 @@
-cd ~/Documents/smontillo.github.io   # if you're not already there
+#!/bin/bash
+set -e  # stop on first error
+
+# go to the repo folder
+cd "/Users/montillo214/Documents/Documents/WebPage/smontillo.github.io"
 
 git add .
-git commit -m "Initial website"
-git push origin main
+
+# Only commit if there are changes
+if ! git diff --cached --quiet; then
+  git commit -m "Auto-export from Bootstrap Studio"
+  git push origin main
+else
+  echo "No changes to commit."
+fi
